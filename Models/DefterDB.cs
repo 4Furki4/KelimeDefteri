39c6befac327cf1c6063e9e374723a6f2eb234e7
4furki4 <muhammedcengiz1@gmail.com>
2022-10-05 09:51:27 +0300

    Libraries installed and main folders added. Defter DB class added

---
+++ Models/DefterDB.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KelimeDefteri.Models
+{
+    public class DefterDB : DbContext
+    {
+        public DefterDB(DbContextOptionsBuilder<DefterDB> optionsBuilder) : base() { }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+
+
+app.UseStaticFiles();
+
+
+app.MapDefaultControllerRoute();
+
+app.Run();
