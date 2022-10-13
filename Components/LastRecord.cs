3746219c550ef6c912f7347f3775bad51878b31a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-10-13 20:39:03 +0300

    Anasayfa(homepage) slightly completed but needs some tunes.

---
+++ Components/LastRecord.cs
@@ -0,0 +1,30 @@
+﻿using KelimeDefteri.Models;
+using KelimeDefteri.ViewModels.Defter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KelimeDefteri.Components
+{
+    public class LastRecord : ViewComponent
+    {
+        private DefterDB context;
+
+        public LastRecord(DefterDB context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            GunlukKayit kayit = await context.GunlukKayitlar
+                .Include(gk => gk.Kelimeler).ThenInclude(k => k.Tanimlar).OrderBy(g=> g.Id)
+                .LastOrDefaultAsync() ?? new GunlukKayit();
+
+            RecordDetailViewModel recordDetailViewModel = new();
+            recordDetailViewModel.date = kayit.date;
+            recordDetailViewModel.Kelimeler = kayit.Kelimeler;
+            recordDetailViewModel.Id = kayit.Id;
+            return View(recordDetailViewModel);
+        }
+    }
+}
