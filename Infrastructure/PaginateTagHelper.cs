87ab0eff091681378d206ff638211c65c5e49524
Furkan <84590614+4Furki4@users.noreply.github.com>
2022-10-16 12:10:23 +0300

    PaginateTagHelper is being prepared.

--- Infrastructure/PaginateTagHelper.cs
+++ Infrastructure/PaginateTagHelper.cs
@@ -11,11 +11,19 @@ namespace KelimeDefteri.Infrastructure
         [ViewContext]
         [HtmlAttributeNotBound]
         public ViewContext? ViewContext { get; set; }
-        public PagingInfo? PageModel { get; set; }
+        public PagingInfo PageModel { get; set; }
         public string? PageAction { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            // tagHelper that helps to create buttons with anchor elements that redirect to paginated url like .../AllRecords/?recordPage=1
+
             TagBuilder tag = new("div");
+
+            for (int i = 0; i < PageModel.TotalPages; i++)
+            {
+                TagBuilder tagBuilder = new("a");
+                tagBuilder.Attributes["asp-action"] = PageAction;
+            }
             
         }
     }
