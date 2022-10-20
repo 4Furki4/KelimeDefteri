d0d2fd5226ddc7245905f8f1f7e754d060b3846f
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-10-20 22:33:49 +0300

    search button fixed and pagination buttons redesigned

--- Infrastructure/PaginateTagHelper.cs
+++ Infrastructure/PaginateTagHelper.cs
@@ -38,7 +38,7 @@ namespace KelimeDefteri.Infrastructure
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { recordPage = i }); // Create url with PageAction and page number
                     if (IsPageClassesEnabled) // Configure classes if enabled
                     {
-                        tag.Attributes["style"] = i != PageModel.CurrentPage ? "background-color:#620277; border-0:#8F00FF" : ""; // Custom background color for selected and normal buttons.
+                        tag.Attributes["style"] = i == PageModel.CurrentPage ? "background-color:#620277; border-0:#8F00FF" : ""; // Custom background color for selected and normal buttons.
                         tag.AddCssClass(PageClass);
                         tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
                     }
