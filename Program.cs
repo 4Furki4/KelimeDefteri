8d36580f4827cd3566decae71a749244b0fecc99
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-11-15 16:14:37 +0300

    .NET 7 changes applied.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-//builder.Services.AddFluentValidation();
+builder.Services.AddFluentValidationAutoValidation();
 
 builder.Services.AddRazorPages();
 
